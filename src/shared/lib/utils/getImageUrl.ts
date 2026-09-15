667f7417dbce19f508cf147b31939c95ecd7eb6d
Farhad <Farhad_>
2026-09-15 22:58:30 +0300

    Обновлён getImageFroms3

--- src/shared/lib/utils/getImageUrl.ts
+++ src/shared/lib/utils/getImageUrl.ts
@@ -3,7 +3,7 @@ const VITE_S3_URL = import.meta.env.VITE_S3_URL
 export const getImageFromS3 = (image: string) => {
     if (!image) return `${VITE_S3_URL}/images/placeholder.png`
     if(VITE_S3_URL) {
-        return `${VITE_S3_URL}/images/${image}.png`
+        return `${VITE_S3_URL}/images/${image}`
     }
     return `/images/${image}.png`
 }
